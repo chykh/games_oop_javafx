3d91cdc2d445ce4bf218b9fb3ff09279edf8bc69
chykh <chykh@mail.ru>
2021-12-07 16:40:40 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,34 +5,35 @@ public class Win {
     }
 
     public static boolean check(int[][] board) {
-        return checkHor(board) || checkVert(board);
-    }
-
-    public static boolean checkHor(int[][] board) {
         boolean rsl = false;
-
-        for (int i = 0; i < board.length; ++i) {
-            if (board[i][0] != 0 && board[i][1] != 0 && board[i][2] != 0
-            && board[i][3] != 0 && board[i][4] != 0) {
-                rsl =     true;
-                break;
-            }
-        }
-
-        return rsl;
-    }
-
-    public static boolean checkVert(int[][] board) {
-        boolean rsl = false;
-
-        for (int i = 0; i < board[0].length; ++i) {
-            if (board[0][i] != 0 && board[1][i] != 0 && board[2][i] != 0
-            && board[3][i] != 0 && board[4][i] != 0) {
+        for (int i = 0; i < board.length; i++) {
+            if (checkHor(board, i) || checkVert(board, i)) {
                 rsl = true;
                 break;
             }
         }
-
         return rsl;
     }
+
+    public static boolean checkHor(int[][] board, int i) {
+        boolean rsl = true;
+        for (int j = 0; j < board[0].length; j++) {
+            if (board[j][i] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkVert(int[][] board, int i) {
+            boolean rsl = true;
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != 1) {
+                    rsl = false;
+                    break;
+                }
+            }
+            return rsl;
+        }
 }
